2d74f54497dbaba766704077550c5bea6c759f14
Faizan Farooq <faixan771@gmail.com>
2024-11-18 00:59:50 +0530

    Apple clone part 04 . Carousel Section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,39 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from '../utils'
+import VideoCarousel from "./VideoCarousel"
 
 const Highlights = () => {
-  return (
-    <div></div>
-  )
+    useGSAP(() => {
+        gsap.to('#title', {
+            opacity: 1,
+            y: 0,
+            text:'white',
+        })
+        gsap.to('.link', {
+            opacity: 1,
+            y: 0,
+            duration:1,
+            stagger:0.25,
+        })
+    }, [])
+    return (
+        <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+            <div className="screen-max-width">
+                <div className="mb-12 w-full md:flex items-end justify-between">
+                    <h2 id="title" className="section-heading">Get The Highlights</h2>
+
+                    <div className="flex flex-wrap items-end gap-5">
+                        <p className="link">Watch the film <img src={watchImg} alt="watch" className="ml-2" /> </p>
+                        {/* duplicate it */}
+                        <p className="link ">Watch the event <img src={rightImg} alt="right" className="ml-2" /> </p>
+                    </div>
+                </div>
+                {/* importing video carousel */}
+                <VideoCarousel/>
+            </div>
+        </section>
+    )
 }
 
 export default Highlights
