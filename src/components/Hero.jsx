3217271db5e15c345c76f59e78afdc432d99961e
Faizan Farooq <faixan771@gmail.com>
2024-11-17 19:14:04 +0530

    Clone part-03 , section 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,28 @@
 import { gsap } from "gsap/gsap-core"
 import { useGSAP } from "@gsap/react"
-import { heroVideo , smallHeroVideo } from '../utils'
+import { heroVideo, smallHeroVideo } from '../utils'
 import { useState, useEffect } from "react"
+import { ScrollTrigger } from "gsap/all"
 
 const Hero = () => {
-const [videoSrc,setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
-// defining function
-const handleVideoSrcSet=()=>{
-    window.innerWidth < 760 ? 
-    setVideoSrc(smallHeroVideo) :
-    setVideoSrc(heroVideo)
-}
-// using useEffent hook
-useEffect(()=>{
-    window.addEventListener('resize',handleVideoSrcSet);
+    // registerPlugin
 
-    return()=> {
-        window.removeEventListener('resize', handleVideoSrcSet)
+    // defining function
+    const handleVideoSrcSet = () => {
+        window.innerWidth < 760 ?
+            setVideoSrc(smallHeroVideo) :
+            setVideoSrc(heroVideo)
     }
-},[])
+    // using useEffent hook
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoSrcSet);
+
+        return () => {
+            window.removeEventListener('resize', handleVideoSrcSet)
+        }
+    }, [])
 
 
     useGSAP(() => {
@@ -29,6 +32,14 @@ useEffect(()=>{
             duration: 2,
 
         })
+        gsap.to('#cta', {
+            opacity: 1,
+            y: -40,
+            delay: 1,
+            duration: 2,
+            ease: "power1.inOut",
+        })
+
     }, [])
 
     return (
@@ -38,9 +49,14 @@ useEffect(()=>{
 
                 <div className="md:w-10/12 w-9/12 ">
                     <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-                        <source src={videoSrc} type="video/mp4"/>
+                        <source src={videoSrc} type="video/mp4" />
                     </video>
                 </div>
+            </div>
+            {/* // new div . cta= call to action*/}
+            <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+                <a href="#highlights" className="btn">Buy </a>
+                <p className="font-normal text-xl ">From $199/month or $999</p>
             </div>
         </section>
     )
