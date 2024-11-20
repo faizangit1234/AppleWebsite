4054546f3236d56d15f1023e4ec535bbff31bce9
Faizan Farooq <faixan771@gmail.com>
2024-11-20 20:41:09 +0530

    Clone Part 06 . Integrated Sentry! a powerful application monitoring and error tracking tool

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
 import Model from "./components/Model"
+import * as Sentry from '@sentry/react'
 
 const App = ()=> {
 
@@ -16,4 +17,4 @@ const App = ()=> {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App); 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://8cd110e9fc55e81c0e0cb0e784cf91b7@o4508330913628160.ingest.us.sentry.io/4508330919198720",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect : React.useEffect
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
